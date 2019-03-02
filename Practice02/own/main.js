baea9554491704a483be294c10950ee71bccfa5d
1998isabel <1998isabel@gmail.com>
2019-03-02 23:25:37 +0800

    new_photo

--- Practice02/own/main.js
+++ Practice02/own/main.js
@@ -1,7 +1,10 @@
-var images = ["https://s.yimg.com/uu/api/res/1.2/._vwGjKrP0YdzL_RTqmgAg--~B/aD0xMDcyO3c9MTYwMDtzbT0xO2FwcGlkPXl0YWNoeW9u/http://media.zenfs.com/zh-Hant-TW/homerun/youthdailynews_517/b3b58dd32da5e94a35666b30e264f717",
+var images = ["http://tvl.ctvba.org.tw/wp-content/uploads/2018/10/-1023-%E5%B9%B3%E9%9D%A2%E5%8C%96-616X290-1-e1540953826903.png",
+    "https://s.yimg.com/uu/api/res/1.2/._vwGjKrP0YdzL_RTqmgAg--~B/aD0xMDcyO3c9MTYwMDtzbT0xO2FwcGlkPXl0YWNoeW9u/http://media.zenfs.com/zh-Hant-TW/homerun/youthdailynews_517/b3b58dd32da5e94a35666b30e264f717",
     "https://obs.line-scdn.net/0hUaMe3ZpgCk5wHCYWBbF1GUpKCSFDcBlNFCpbTTNyVHoNLhoeRS0VIFMeUSxaeU0QHnJCKlweEX9bKh4QGy4V/w644",
     "https://c1.staticflickr.com/5/4771/40686350291_b7b5b388e1_b.jpg",
-    "https://cdn2.ettoday.net/images/2743/d2743589.jpg"
+    "http://media.pbplus.me/wp-content/uploads/2017/09/21082897_1638478296172035_1713942032626361786_o.jpg",
+    "https://volsports.co/wp-content/uploads/2018/12/DSC09756.jpg",
+    "https://volsports.co/wp-content/uploads/2018/12/cdbca54d7bcda8c2583393d08523f72fb_14906360_181201_0004.jpg"
 ]
 
 var targetElement = document.getElementById("display");
@@ -12,15 +15,17 @@ next_button.onclick = nextImage;
 var i = 0;
 
 function previousImage() {
-    if( i == 0 ){
-        prev_button.className = "disabled";
-    }
-    else if(i == (images.length-1)) {
-        next_button.className = "image-viewer__button";
-        i = i - 1;
+    i = i - 1;
+    if( i < 0 ){
+        i = 0;
     }
     else {
-        i = i - 1;
+        if(i == 0){
+            prev_button.className = "disabled";
+        }
+        if(i == (images.length-2)) {
+            next_button.className = "image-viewer__button";
+        }
         targetElement.setAttribute ('src', 'images/loading.gif');
         targetElement.setAttribute ('src', images[i]);
     }
@@ -28,12 +33,17 @@ function previousImage() {
 
 function nextImage() {
     i = i + 1;
-    if (i >= 0 && i < images.length) {
-        prev_button.className = "image-viewer__button";
-        targetElement.setAttribute ('src', images[i]);
+    if (i > (images.length - 1)) {
+        i = images.length - 1
     }
-    else if(i == images.length) {
-        i = i - 1;
-        next_button.className = "disabled";
+    else {
+        if(i == (images.length - 1)){
+            next_button.className = "disabled";
+        }
+        if(i == 1) {
+            prev_button.className = "image-viewer__button";
+        }
+        targetElement.setAttribute ('src', 'images/loading.gif');
+        targetElement.setAttribute ('src', images[i]);
     }
 }
